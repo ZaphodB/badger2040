2c0e7902aed990c80e410945562bab9facbfbd31
Stefan Schmidt <sschmidt@wavecon.de>
2023-08-21 19:01:22 +0200

    testing

--- badger_os/examples/wifi_scan.py
+++ badger_os/examples/wifi_scan.py
@@ -1,6 +1,18 @@
 import badger2040
 from badger2040 import WIDTH
 import network
+from enum import Enum
+
+class Security(Enum):
+    'open' = 0
+    'WEP' = 1
+    'WPA-PSK' = 2
+    'WPA2-PSK' = 3
+    'WPA/WPA2-PSK' = 4
+
+class Visibility(Enum):
+    'visible' = 0
+    'hidden' = 1
 
 TEXT_SIZE = 1
 LINE_HEIGHT = 16
@@ -15,14 +27,18 @@ display.clear()
 display.set_pen(0)
 display.rectangle(0, 0, WIDTH, 20)
 display.set_pen(0)
+display.set_font('bitmap8')
 
 y = 35 + int(LINE_HEIGHT / 2)
 
 nic = network.WLAN(network.STA_IF)
 nic.active(True)
-for net in nic.scan():
+nets = nic.scan()
+# sort by 4th item (RSSI) descending
+nets.sort(key=lambda x: x[3], reverse=True)
+for net in nets:
     ssid, bssid, channel, RSSI, security, hidden = net
-    display.text("> {}".format(ssid), 0, y, WIDTH)
+    display.text("{} {} {} {}".format(Visibility(hidden).name, str(RSSI), ssid.decode("utf-8"), Security(security).name), 0, y, WIDTH)
     y += LINE_HEIGHT
 
 display.update()
