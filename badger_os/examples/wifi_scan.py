981727b92d537594ad4c2190a0019fba7909b0e2
Stefan Schmidt <sschmidt@wavecon.de>
2023-08-21 19:56:00 +0200

    hmm

--- badger_os/examples/wifi_scan.py
+++ badger_os/examples/wifi_scan.py
@@ -4,17 +4,17 @@ import network
 from enum import Enum
 
 
-class Security(Enum):
-    open = 0
-    WEP = 1
-    WPA_PSK = 2
-    WPA2_PSK = 3
-    WPA_WPA2_PSK = 4
-
-
-class Visibility(Enum):
-    visible = 0
-    hidden = 1
+# class Security(Enum):
+#     open = 0
+#     WEP = 1
+#     WPA_PSK = 2
+#     WPA2_PSK = 3
+#     WPA_WPA2_PSK = 4
+# 
+# 
+# class Visibility(Enum):
+#     visible = 0
+#     hidden = 1
 
 
 TEXT_SIZE = 1
@@ -30,7 +30,7 @@ display.clear()
 display.set_pen(0)
 display.rectangle(0, 0, WIDTH, 20)
 display.set_pen(0)
-# display.set_font('bitmap8')
+display.set_font('bitmap8')
 
 y = 35 + int(LINE_HEIGHT / 2)
 
@@ -41,7 +41,8 @@ nets = nic.scan()
 nets.sort(key=lambda x: x[3], reverse=True)
 for net in nets:
     ssid, bssid, channel, RSSI, security, hidden = net
-    display.text("{} {} {} {}".format(Visibility(hidden).name, str(RSSI), ssid.decode("utf-8"), Security(security).name), 0, y, WIDTH)
+    # display.text("{} {} {} {}".format(Visibility(hidden).name, str(RSSI), ssid.decode("utf-8"), Security(security).name), 0, y, WIDTH)
+    display.text("{} {} {} {}".format(str(hidden), str(RSSI), ssid.decode("utf-8"), str(security)), 0, y, WIDTH)
     y += LINE_HEIGHT
 
 display.update()
